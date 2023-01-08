7235238f85a6c030394131de9d4b4b8fba51452b
Ling T <lxiutran@gmail.com>
2023-01-07 19:00:50 -0500

    [#38] Apply typescript templates for service workers instead

--- client/src/serviceWorkerRegistration.ts
+++ client/src/serviceWorkerRegistration.ts
@@ -1,6 +1,7 @@
-// Reference for content in file:
-// https://github.com/cra-template/pwa/blob/main/packages/cra-template-pwa/template/src/serviceWorkerRegistration.js
-
+/*
+Source template code from CRA-PWA-typescript package:
+https://github.com/cra-template/pwa/blob/main/packages/cra-template-pwa-typescript/template/src/serviceWorkerRegistration.ts
+ */
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
@@ -21,7 +22,12 @@ const isLocalhost = Boolean(
       window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
   );
   
-  export function register(config) {
+  type Config = {
+    onSuccess?: (registration: ServiceWorkerRegistration) => void;
+    onUpdate?: (registration: ServiceWorkerRegistration) => void;
+  };
+  
+  export function register(config?: Config) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
       // The URL constructor is available in all browsers that support SW.
       const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -55,7 +61,7 @@ const isLocalhost = Boolean(
     }
   }
   
-  function registerValidSW(swUrl, config) {
+  function registerValidSW(swUrl: string, config?: Config) {
     navigator.serviceWorker
       .register(swUrl)
       .then((registration) => {
@@ -99,7 +105,7 @@ const isLocalhost = Boolean(
       });
   }
   
-  function checkValidServiceWorker(swUrl, config) {
+  function checkValidServiceWorker(swUrl: string, config?: Config) {
     // Check if the service worker can be found. If it can't reload the page.
     fetch(swUrl, {
       headers: { 'Service-Worker': 'script' },
