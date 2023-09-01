4984477ee44c3cfd050eb9d78b640e350fe9ca4e
Ling T <xiaolingbao430@gmail.com>
2023-08-31 23:38:13 -0400

    [#8] WIP - building react test

--- client/src/hello_world/HelloWorld.test.tsx
+++ client/src/hello_world/HelloWorld.test.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import HelloWorld from './HelloWorld';
 
 
 const server = setupServer(
     rest.get('/test', (req, res, ctx) => {
-        return res(ctx.json({ result: 'Hello, world! Let the cosmos pair' }))
+        return res(
+            ctx.status(200),
+            ctx.json({ result: 'Hello, world! Let the cosmos pair' }),
+        )
     }),
 )
 
 describe('HelloWorld', () => {
-    beforeAll(() => server.listen());
+    beforeAll(() => server.listen({ onUnhandledRequest: "bypass" }));
     afterEach(() => server.resetHandlers());
     afterAll(() => server.close());
 
     it('renders hello world message', async () => {
         render(<HelloWorld />);
+        // not resolving for some reason
         await waitFor(() => {
-            expect(screen.getByTestId('hello')).toHaveTextContent(/Hello, world! Let the cosmos pair/i);
-        })
+            screen.getByTestId('hello');
+        });
+        expect(screen.getByTestId('hello')).toHaveTextContent(/Hello, world! Let the cosmos pair/i);
     });
 });
